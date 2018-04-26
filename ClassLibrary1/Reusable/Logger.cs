cd4d68391b03af72089706b7e3f02d7c0808905f
alessandro <alessandro@MSI>
2018-04-26 00:26:12 -0500

    ObjectPool - Fix failing build

---
+++ ClassLibrary1/Reusable/Logger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectPool.Reusable
+{
+    public class Logger
+    {
+        public int ID { get; private set; }
+
+        public Logger(int id)
+        {
+            ID = id;
+        }
+    }
+}
